a29c862798526d8f9c9c8c4c7a6f01b461b7938c
Mating5686 <144844721+Mating5686@users.noreply.github.com>
2025-07-25 19:18:02 +0330

    BOT.py

--- BOT.py
+++ BOT.py
@@ -943,10 +943,13 @@ def main():
     app.add_handler(CallbackQueryHandler(admin_panel_callback, pattern="^(ban_user|unban_user|bot_stats)$"))
     app.add_handler(CallbackQueryHandler(button))
 
+
+    app.add_handler(MessageHandler(filters.TEXT & filters.ChatType.GROUPS, anti_link_handler))
+
     app.add_handler(MessageHandler(filters.TEXT & filters.ChatType.GROUPS, handle_user_msg))
     app.add_handler(MessageHandler(filters.TEXT & filters.ChatType.PRIVATE, handle_user_msg))
+    
     app.add_handler(MessageHandler(filters.TEXT & filters.User(user_id=ADMIN_ID), admin_action_handler))
-    app.add_handler(MessageHandler(filters.TEXT & filters.ChatType.GROUPS, anti_link_handler))
     app.add_handler(MessageHandler(filters.TEXT & filters.ChatType.PRIVATE, handle_user_msg))
     app.add_handler(MessageHandler(filters.REPLY, handle_admin_reply))
     app.add_handler(MessageHandler(
